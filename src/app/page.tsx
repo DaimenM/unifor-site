16574b4953fa5026cd47e4305f43a5c85b2064b1
Kay Roye <05kalusss@gmail.com>
2024-12-11 21:05:14 -0500

    Changed title on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,7 @@ import SearchBar from "@/components/search-bar";
 export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold text-red-600">District 300</h1>
-      <h2 className="text-2l font-bold text-gray-600 mb-8">Unifor Local 2002</h2>
+      <h1 className="text-4xl font-bold text-red-600 mb-6">Welcome to District 300!</h1>
       <SearchBar />
       <ArticleList articles={articles} />
     </main>
