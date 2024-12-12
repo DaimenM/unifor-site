99bd0c981b295ec24a150cee5df12af303dbf2fe
Daimen Miller <mdaimen14@gmail.com>
2024-12-11 20:43:07 -0500

    Edited banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ import SearchBar from "@/components/search-bar";
 
 export default function Home() {
   return (
-    <main className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold text-red-600">District 300</h1>
-      <h2 className="text-2l font-bold text-gray-600 mb-8">Unifor Local 2002</h2>
-      <SearchBar />
-      <ArticleList articles={articles} />
+    <main className="w-full px-4 py-8">
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-4xl font-bold text-red-600">District 300</h1>
+        <h2 className="text-2l font-bold text-gray-600 mb-8">Unifor Local 2002</h2>
+        <SearchBar />
+        <ArticleList articles={articles} />
+      </div>
     </main>
   );
 }
-
